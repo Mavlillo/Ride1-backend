5be88f935ce15bcb2b6ef4eea6e5a45efdf8b580
mavlo <you@example.com>
2022-07-14 16:07:31 -0400

    backend

---
+++ src/models/Task.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  title: 'string',
+  content: 'string',
+  value: 'string',
+  done: 'boolean',
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+});
+
+module.exports = mongoose.model('Task', schema);
---
+++ src/validators/taskSchema.js
@@ -0,0 +1,12 @@
+const Joi = require('joi')
+
+const taskCreationSchema = Joi.object().keys({
+  title: Joi.string().required(),
+  content: Joi.string().required(),
+  value: Joi.string().required(),
+  done: Joi.boolean()
+})
+
+module.exports = {
+  taskCreationSchema
+}
